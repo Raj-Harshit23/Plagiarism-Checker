e11817bf5ed0cce054551e4571b2ac01dc27934f
Sagnik-Nandi <144457537+Sagnik-Nandi@users.noreply.github.com>
2024-11-03 02:17:09 +0530

    Mast comment dal diya

--- plagiarism_checker/phase1/match_submissions_final.hpp
+++ plagiarism_checker/phase1/match_submissions_final.hpp
@@ -17,6 +17,14 @@
 //-------------------------------------------------
 //------------------------------------------------
 
+/*
+* The present code utilizes KMP to find all disjoint exact matches between given vectors.
+* Also it uses dynamic programming to find the longest approximate match.
+* Based on these two results we judge if plagiarism is present or not.
+*/
+
+
+
 // Uses Knuth-Morris-Pratt algorithm to find exact matching
 class KMP
 {
@@ -24,7 +32,7 @@ private:
     // HELPER Functions
     // To avoid overlapping between the matches found in submission2 (the text vector to be matched)
     // Hence we find the unmatched part, which can accomodate the present pattern
-    static int findNextUnmatchedPart(int patsize, int txtsize, int start, const std::unordered_set<int> &matchedIndices2)
+    static int findNextUnmatchedPart(int patsize, int txtsize, int start, const std::unordered_set<int> &matchedIndices)
     {
         bool found = 0;
         int i = start;
@@ -32,21 +40,21 @@ private:
         while (!found && i < txtsize)
         {
             // if current pattern were to be matched, the end index of pattern must be unmatched
-            if (matchedIndices2.count(i + patsize - 1))        
+            if (matchedIndices.count(i + patsize - 1))        
             {
                 i = i + patsize;
                 continue;
             }
 
             // exhaustive check to find if any pattern matching has already been done, in between start and end index
-            else if (matchedIndices2.count(i + 10))
+            else if (matchedIndices.count(i + 10))
             {
                 i = i + 11;
                 continue;
             }
 
             // if present index is already matched
-            else if (matchedIndices2.count(i))
+            else if (matchedIndices.count(i))
             {
                 i++;
                 continue;
@@ -74,15 +82,10 @@ private:
             {
                 h[i] = j;
                 // precompute for next iteration
-                while (j >= 0 && pat[i] != pat[j])
-                {
-                    j = h[j];
-                }
+                while (j >= 0 && pat[i] != pat[j]) j = h[j];
             }
-            else
-            {
-                h[i] = h[j];
-            }
+            else h[i] = h[j];
+
             i++;
             j++;
         }
@@ -91,12 +94,15 @@ private:
 
 public:
     // MAIN Function
-    static int KMPSearch(const std::vector<int> &pat, const std::vector<int> &txt, std::unordered_set<int> &matchedIndices2)
+    static int KMPSearch(const std::vector<int> &pat, const std::vector<int> &txt, std::unordered_set<int> &matchedIndices)
     {
         int j = 0;
         int m = pat.size();
         int n = txt.size();
-        int i = findNextUnmatchedPart(m, n, 0, matchedIndices2);
+
+        // This function finds the starting index first contiguous part of txt 
+        // which is atleast the size of pat and completely unmatched
+        int i = findNextUnmatchedPart(m, n, 0, matchedIndices);
         if (i == -1) // Means No remaining place found, where matching can be done.
         {
             return 0;
@@ -105,31 +111,30 @@ public:
         computeKMPTable(pat, m, h);
         while (i < n)
         {
-            if (matchedIndices2.count(i)) //
+            // If the current index is already matched, we need to find next unmatched part
+            // and then restart kmp by setting j=0
+            if (matchedIndices.count(i)) 
             {
-                i = findNextUnmatchedPart(m, n, i, matchedIndices2);
+                i = findNextUnmatchedPart(m, n, i, matchedIndices);
                 j = 0;
                 continue;
             }
-            // printf("Inside search loop\n, i=%d, j=%d", i, j);
             if (pat[j] == txt[i])
             {
                 i++;
                 j++;
-                if (j == m) // j==|Pat|
+                if (j == m) 
                 {
-                    // Adding the matched pattern indices in matchedIndices2
-                    for (int k = i - j; k < i; k++)
-                    {
-                        matchedIndices2.insert(k);
-                    }
+                    // Adding the matched pattern indices in matchedIndices
+                    for (int k = i - j; k < i; k++) matchedIndices.insert(k);
                     return j; // returns the length of the pattern matched
                 }
             }
             else
             {
+                // shifting j to the point where the matched portion repeats
                 j = h[j];
-                if (j < 0)
+                if (j < 0) // no portion of pattern is matched, move index
                 {
                     i++;
                     j++;
@@ -148,12 +153,15 @@ class LCS
 {
 public:
     // MAIN Function
+    // Returns length of longest match and starting indices in both vectors
     static std::vector<int> longest_approximate_match(std::vector<int> &vec1, std::vector<int> &vec2)
     {
         int m = vec1.size();
         int n = vec2.size();
-        // Maintain a table for calculating dynamic programming
-        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+
+        // Maintain a table for calculating LCS using dynamic programming
+        // Ref: https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+        std::vector<std::vector<int>> dp(m+1, std::vector<int>(n + 1, 0));
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (vec1[i - 1] == vec2[j - 1]) {
@@ -165,69 +173,95 @@ public:
         }
 
         // Backtrack to find the LCS elements and their indices
-        // std::vector<std::tuple<int, int, int>> lcsWithIndices;
+        // Ref: https://www.geeksforgeeks.org/printing-longest-common-subsequence/
         std::vector<int> ind1, ind2;
         int i = m, j = n;
         while (i > 0 && j > 0) {
-            if (vec1[i - 1] == vec2[j - 1]) {
-                // lcsWithIndices.emplace_back(vec1[i - 1], i - 1, j - 1); // Append value and indices to LCS
+            // If current character in vec1 and vec2 are same, then
+            // current character is part of LCS
+            if (vec1[i - 1] == vec2[j - 1]) { 
                 ind1.push_back(i-1);
                 ind2.push_back(j-1);
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            } 
+            // If not same, then find the larger of two and
+            // go in the direction of larger value
+            else if (dp[i - 1][j] > dp[i][j - 1]) {
                 i--; // Move up
             } else {
                 j--; // Move left
             }
         }
 
+
         // Since we constructed lcs backwards, reverse it
         std::reverse(ind1.begin(), ind1.end());
         std::reverse(ind2.begin(), ind2.end());
 
-        // For 80% check
-        int ni=ind1.size();
+        // Answers : length, start-index1, start-index2
         int ans1=0;
         int ans2=0;
         int ans3=0;
 
-        // 
-        for(int k=0; k<ni; k++){
-            for(int h=ni-1; h>k; h--){
+        // Look for the largest subsequence first
+        int indSize=ind1.size();
+        for(int k=0; k<indSize; k++){
+            for(int h=indSize-1; h>k; h--){
+                // If match found is small, discard it and continue to next iteration
                 int minStringLen=std::min((ind1[h]-ind1[k]),(ind2[h]-ind2[k]));
-                if(minStringLen<30)
-                {
-                    break;
-                }
+                if(minStringLen<30) break;
+                
                 int maxStringLen=std::max((ind1[h]-ind1[k]),(ind2[h]-ind2[k]));
                 double fracMatch=double(h-k+1)/(maxStringLen+1);
-                if(fracMatch>0.95)
+
+                // Even if match found is greater than 80%, we should find the longest approximate match,
+                // by extending it as much as possible such that accuracy remains greater than 80%
+                int ans=std::min(std::min((int)((maxStringLen+1)*(fracMatch/0.8)),m),n); 
+
+                // Fraction of matching subsequence should be at least 80% of longer substring    
+                if(fracMatch>0.8 && ans>ans1)
                 {
-                    int ans=std::min(std::min((int)((maxStringLen+1)*(fracMatch/0.8)),m-ind1[k]),n-ind2[k]);     //the max matching length can be scaled such that accuracy remains greater than 80%
-                    // std::cout<<fracMatch<<" "<<ans<<" "<<maxStringLen<<std::endl;
-                    if(ans>ans1)
-                    {
-                        ans1=ans;
-                        ans2=ind1[k];
-                        ans3=ind2[k];
-                    }
+                    ans1=ans;
+                    ans2=ind1[k];
+                    ans3=ind2[k];
                 }
             }
         }
         return {ans1,ans2,ans3};
-
-        
-        // for(auto k: lcsWithIndices){
-        //     std::cout<<std::get<0>(k)<<" "<<std::get<1>(k)<<" "<<std::get<2>(k)<<"     ";
-        // }
-        // auto [a,b,c]=lcsWithIndices[0];
-        // return {a,b,c};
-
     }
 };
 //---------------------------------------
 //---------------------------------------
+
+// HELPER Function for exact matching
+// Returns total match length 
+int total_match(const std::vector<int> &submission1, const std::vector<int> &submission2){
+    // MatchedIndices required to avoid non overlapping
+    std::unordered_set<int> matchedIndices;
+
+    int totalMatch = 0;
+
+    for (int i = 0; i < (int)submission1.size(); ++i)
+    {
+        // Check patterns of length between 20 and 10 (start from larger patterns first)
+        for (int len = 20; len >= 10; --len)
+        {
+            if (i + len > submission1.size()) continue; // Remove out of bound errors
+
+            std::vector<int> pattern(submission1.begin() + i, submission1.begin() + i + len);
+            int match = KMP::KMPSearch(pattern, submission2, matchedIndices);
+            totalMatch += match;
+
+            if (match > 0)
+            {
+                i=i+len-1;
+                break; // Skip smaller patterns if a larger one is found
+            }
+        }
+    }
+    return totalMatch;
+}
 
 // MAIN Function of this file
 // Finds matches between two tokenized files
@@ -238,55 +272,17 @@ std::array<int, 5> match_submissions(std::vector<int> &submission1,
     if (submission1.size() > submission2.size())
         return match_submissions(submission2, submission1);
 
-    // matchedIndices required to avoid non overlapping
-    std::unordered_set<int> matchedIndices1;
-    std::unordered_set<int> matchedIndices2;
-
-    // Initialize total match for exact matching
-    int totalMatch = 0;
-
-    for (int i = 0; i < (int)submission1.size(); ++i)
-    {
-        // If index `i` is part of an already matched pattern, skip it
-        if (matchedIndices1.count(i))
-        {
-            continue;
-        }
-
-        // Check patterns of length between 20 and 10 (start from larger patterns first)
-        for (int len = 20; len >= 10; --len)
-        {
-            if (i + len > submission1.size())
-                continue; // Skip out-of-bound patterns
-
-            // Search for the pattern
-            std::vector<int> pattern(submission1.begin() + i, submission1.begin() + i + len);
-            int match = KMP::KMPSearch(pattern, submission2, matchedIndices2);
-            totalMatch += match;
-
-            if (match > 0)
-            {
-                // Mark all indices within this pattern as "matched"
-                for (int j = i; j < i + len; ++j)
-                {
-                    matchedIndices1.insert(j);
-                }
-                break; // Skip smaller patterns if a larger one is found
-            }
-        }
-    }
-
-    // Parameters for
-    int min_match_len = 30;                                                                         // Minimum length to consider as a match
-    int max_mismatches = std::min(50, int(0.1 * std::min(submission2.size(), submission1.size()))); // Allow some mismatch in a subsequence
+    // Exact Matching function
+    int totalMatch=total_match(submission1, submission2);
 
     // Approximate matching function
     std::vector<int> approxMatch = LCS::longest_approximate_match(submission1, submission2);
-    // std::vector<int> approxMatch=LSH::longest_approximate_match(submission1, submission2);
 
-    // Report plag if more than half of the tokens are same
-    double fracPlag = totalMatch / (double(std::max(submission1.size(), submission2.size())));
-    int hasPlag = (fracPlag > 0.5);
+    // Report plag based on total and approx match
+    double fracExactPlag = totalMatch / (double(std::max(submission1.size(), submission2.size())));
+    double fracApproxPlag = approxMatch[0]/(double(std::max(submission1.size(), submission2.size())));
+    int hasPlag = (fracExactPlag > 0.2 || fracApproxPlag > 0.5) ;
+
     std::array<int, 5> result = {hasPlag, totalMatch, approxMatch[0], approxMatch[1], approxMatch[2]};
     return result;
 }
