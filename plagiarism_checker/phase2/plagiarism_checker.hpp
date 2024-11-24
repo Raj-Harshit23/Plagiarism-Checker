e6df1a4401068b4d62cca7c8f72c89bb6f50585e
Harshit Raj <152403760+Raj-Harshit23@users.noreply.github.com>
2024-11-24 16:09:41 +0530

    Update plagiarism_checker.hpp

--- plagiarism_checker/phase2/plagiarism_checker.hpp
+++ plagiarism_checker/phase2/plagiarism_checker.hpp
@@ -1,10 +1,12 @@
 #include "structures.hpp"
+// -----------------------------------------------------------------------------
 #include<set>
 #include<unordered_map>
 #include<thread>
 #include<mutex>
-// -----------------------------------------------------------------------------
-
+#include<chrono>
+#include<queue>
+#include<bitset>
 // You are free to add any STL includes above this comment, below the --line--.
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
@@ -15,18 +17,34 @@ class plagiarism_checker_t {
     // You should NOT modify the public interface of this class.
 public:
     plagiarism_checker_t(void);
-    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions);
+    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions){
+    };
     ~plagiarism_checker_t(void);
     void add_submission(std::shared_ptr<submission_t> __submission);
 
 protected:
     // TODO: Add members and function signatures here
     std::vector<std::shared_ptr<submission_t>> submissions;
-    std::vector<std::chrono::system_clock::time_point> timestamps;
-    void check_plag(std::shared_ptr<submission_t> __submission);
+    std::vector<double> timestamps;
+    // Should take difference for timestamps?store initial time for constructor
+    std::queue<std::pair<std::shared_ptr<submission_t>, double>> submission_queue; // Queue for submissions
+    std::mutex m;  // Mutex to protect access to the queue
+    std::thread worker_thread; // Single worker thread
+    bool thread_running; // Flag to check if thread is running
+    void check_plag(std::shared_ptr<submission_t> __submission, double start_time);
+    void len15check(std::vector<int>& submission, double start_time);
+    void len75check(std::vector<int>& submission, double start_time);
+    void patchCheck(std::vector<int>& submission, double start_time);
+
+    // to check if the file is already plagged
+    std::vector<bool> plagged;
+
+    // Queue of submissions in present 1 second window
+    // updated whenever a new submission is received
+    std::queue<std::pair<std::shared_ptr<submission_t>, double>> present_window;
+    std::thread t;
     std::vector<int> tokenize_file(const std::string& file_name);
     std::vector<std::bitset<400000>> bitsets;
-    std::thread t();
-    t.detach();
+    
     // End TODO
 };
